51ebc8838a05b3a01bbdc1977f0daba68556136e
aimery.wei <aimery.wei@gmail.com>
2018-07-15 22:32:54 +0800

    add function_template and it vs function overload

--- cpp_learning/function_template.cpp
+++ cpp_learning/function_template.cpp
@@ -1,9 +1,15 @@
 
 #include "iostream"
+#include <string.h>
+
 #include "string"
 
 using namespace std;
 
+/*
+ C++ compiles template first, then it compiles specific function by given type, when calling it.
+ So it compiles at least twice.
+ */
 
 template <typename T1, typename T2>
 T2 Swap_Func_Tmp(T1 & a, T1 & b)
@@ -24,6 +30,40 @@ void Swap_Func_Tmp2(T1 & a, T1 & b)
     tmp = a;
     a = b;
     b = tmp;
+}
+
+template <typename T1, typename T2>
+void Swap_Func_Tmp2(T1 & a, T2 & b)
+{
+    T1 tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void Swap_Func_Tmp2(int & a, char & b)
+{
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+    cout <<"This is a normal function not a template" << endl;
+}
+
+template <typename T>
+void Sort_Array(T * array, int len)
+{
+    int i = 0;
+    int j = 0;
+    for(; i<len; i++)
+    {
+        for(j=i+i; j<len; j++)
+        {
+            if(array[j]<array[i]){
+                Swap_Func_Tmp2<T>(array[i], array[j]);
+            }
+        }
+    }
 }
 
 
@@ -47,6 +87,39 @@ int main()
     
     Swap_Func_Tmp2(a, b); //it can derive simple type by itself
     cout << a << " and " << b << endl;
+    
+    string strs[] = {str1, str2};
+    
+    //Sort_Array<string>(str1, str1.length()); //it couldn't accept string type directly, candidate function not viable: no known conversion from 'string' ...
+    
+    Sort_Array<char>((char *)(str1.c_str()), str1.length());
+    cout << str1.length() << endl;
+    cout << str1 << endl;
+    
+    cout << strs[0] << " and " << strs[1] << endl;
+    Sort_Array<string>(strs, sizeof(strs)/sizeof(strs[0]));
+    cout << strs[0] << " and " << strs[1] << endl;
+    
+    char array[] = "aojfosiergisergj";
+    Sort_Array<char>(array, strlen(array));
+    cout << array << endl;
+    
+    int x = 100;
+    char y = 'a';
+    //Swap_Func_Tmp2(x, y); //error: no matching function for call to 'Swap_Func_Tmp2'
+    //Swap_Func_Tmp2<int>(x, y); //error: no matching function for call to 'Swap_Func_Tmp2'
+
+    /*Without function overload, function template won't convert data type implicitly and defaultly !!!*/
+    
+    
+    Swap_Func_Tmp2<int>(x, y);  //function overload takes place
+    cout << x << " " << y << endl;
+    
+    Swap_Func_Tmp2(x, y); //normal function is perior to call, if it can
+    cout << x << " " << y << endl;
+    
+    Swap_Func_Tmp2<>(x, y); //by <>, function template is called explictly
+    cout << x << " " << y << endl;
     
     return 0;
 }
