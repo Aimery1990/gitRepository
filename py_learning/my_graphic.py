c212cb654d8051dad8302d62551d5d76083caf11
aimery.wei <aimery.wei@gmail.com>
2018-10-02 19:45:44 +0800

    set graphics.py into whole system and set local graphics.py into backup

--- py_learning/my_graphic.py
+++ py_learning/my_graphic.py
@@ -4,15 +4,22 @@ from graphics import *
 
 win = GraphWin()
 
-time.sleep(10)
+time.sleep(2)
 leftEye = Circle(Point(80, 50), 5)
 leftEye.setFill("yellow")
 leftEye.setOutline('red')
+leftEye.draw(win)
 
 rightEye = leftEye  # this is not a copy action, rightEye is a reference of leftEye
+#rightEye.draw(win)
 time.sleep(3)
 rightEye.move(20, 0) # both 2 eyes are moving
 
-time.sleep(10)
+rightEye2 = leftEye.clone()
+rightEye2.draw(win)
+rightEye2.move(-20, 0)
+
+
+time.sleep(3)
 
 win.close()
